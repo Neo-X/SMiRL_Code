cf0fc6b7a55d0be3e93731c1a4fc0c7352aba3cc
Glen Berseth <Neo-X@users.noreply.github.com>
2022-11-13 13:21:43 -0500

    Create install_docker.sh
    Add docker setup.

---
+++ install_docker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+###
+### Print list of resouce links so you can quickly look through them
+###
+
+sudo apt-get -y install curl
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+apt-cache policy docker-ce
+
+sudo apt-get install -y docker-ce
+
+sudo systemctl status docker
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update
+
+sudo apt-get install -y nvidia-docker2
+
+sudo usermod -aG docker ${USER}
+
+su - ${USER}
+
+id -nG
+
+sudo systemctl restart docker
+
+sudo docker run --rm --gpus all nvidia/cuda:10.1-base nvidia-smi
+
+### Setup GCP authorization
